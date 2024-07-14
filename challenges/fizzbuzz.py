3aac451666e0e447539c4d85907ca5b46d685508
Raven Ravener <ravener.anime@gmail.com>
2024-07-14 21:45:02 +0500

    style: black formatting

--- challenges/fizzbuzz.py
+++ challenges/fizzbuzz.py
@@ -20,13 +20,11 @@ def fizzbuzz(n: int) -> list[str]:
     :returns: A list containing the fizzbuzz sequence.
     """
     return [
-        "FizzBuzz"
-        if i % 3 == 0 and i % 5 == 0
-        else "Fizz"
-        if i % 3 == 0
-        else "Buzz"
-        if i % 5 == 0
-        else str(i)
+        (
+            "FizzBuzz"
+            if i % 3 == 0 and i % 5 == 0
+            else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+        )
         for i in range(1, n + 1)
     ]
 
